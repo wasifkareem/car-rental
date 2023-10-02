b395f16b73184c75fe780d90d8824f1cfe224d84
wasif kareem <wasifkareem2762@gmail.com>
2023-10-03 00:15:23 +0530

    UI fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
   };
   return (
     <>
-      <section className=" justify-end flex items-center right-0 bg-[#edf0f6] border border-gray-300 px-5 h-14  rounded-b ">
+      <section className=" justify-end flex items-center right-0 bg-[#edf0f6] border border-gray-300 px-5 h-14   rounded-b ">
         <button
           onClick={handleExplore}
           className="flex mr-6 border  rounded shadow-sm shadow-gray-600 font-semibold bg-inherit hover:bg-white duration-300 p-2 px-3 text-xs"
@@ -20,10 +20,10 @@ const Home = () => {
         </button>
       </section>
       <div className="  flex justify-center  flex-col text-center h-[100vh] bg-[#edf0f6] ">
-        <p className="font-extrabold text-transparent text-5xl  bg-clip-text bg-gradient-to-r from-red-400 to-gray-600 ">
+        <p className="font-extrabold text-transparent text-5xl pb-2  bg-clip-text bg-gradient-to-r from-red-400 to-gray-600 ">
           Welcome to Stazi Technologies
         </p>
-        <p className=" text-xl mb-2">
+        <p className=" text-xl  mb-2">
           Revolutionizing car rental space in India
         </p>
         <div className=" flex justify-center ">
@@ -32,7 +32,7 @@ const Home = () => {
         <div>
           <button
             onClick={handleExplore}
-            className=" border-2 border-l-black border-b-black border-t-gray-600 border-r-gray-600 px-32  shadow mb-7 shadow-black text-2xl py-5 text-black font-semibold  w-fit"
+            className=" border-2 border-l-black border-b-black border-t-gray-600 border-r-gray-600 px-32  shadow mb-16 shadow-black text-2xl py-5 text-black font-semibold  w-fit"
           >
             Explore Now
           </button>
